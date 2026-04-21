35fbe5f9f4f1727d34af91e336e5ab50bf7a9d86
Dilliya <dilusha.37.writer@gmail.com>
2026-04-21 18:32:57 +0400

    little better

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let count = 1;
-
-document.querySelector(".add-button").addEventListener('click', () => {
-    count += 1;
-    const original = document.querySelector(".beverage");
-    const clone = original.cloneNode(true);
-    clone,id = `beverage${count}`;
-    clone.querySelector('.beverage-count').textContent = `Напиток №${count}`;
-    document.getElementsByTagName('form')[0].appendChild(clone);
-})
+for (const addButton of document.getElementsByClassName("add-button")) {
+    addButton.addEventListener('click', () => {
+        count += 1;
+        const original = document.getElementsByTagName("form")[0];
+        const clone = original.cloneNode(true);
+        clone, id = `beverage${count}`;
+        clone.querySelector('.beverage-count').textContent = `Напиток №${count}`;
+        document.getElementsByTagName('body')[0].appendChild(clone);
+    })
+}
